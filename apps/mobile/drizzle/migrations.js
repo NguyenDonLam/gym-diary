007034fcb74965961440a4436188e273c6b8e524
NguyenDonLam <donlamn@student.unimelb.edu.au>
2025-12-19 01:43:50 +1100

    feat(Exercise Statistic): add migrations

--- apps/mobile/drizzle/migrations.js
+++ apps/mobile/drizzle/migrations.js
@@ -7,6 +7,7 @@ import m0002 from './0002_parallel_hardball.sql';
 import m0003 from './0003_uneven_gamma_corps.sql';
 import m0004 from './0004_fair_sauron.sql';
 import m0005 from './0005_cool_adam_warlock.sql';
+import m0006 from './0006_narrow_peter_quill.sql';
 
   export default {
     journal,
@@ -16,7 +17,8 @@ m0001,
 m0002,
 m0003,
 m0004,
-m0005
+m0005,
+m0006
     }
   }
   
