3c4b07f96fe23ea6d8921084f1a2cf6d42cf9de3
NguyenDonLam <donlamn@student.unimelb.edu.au>
2025-12-27 13:06:34 +1100

    feat: migration files

--- apps/mobile/drizzle/migrations.js
+++ apps/mobile/drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_nappy_scarlet_spider.sql';
 import m0001 from './0001_foamy_giant_girl.sql';
+import m0002 from './0002_short_infant_terrible.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
